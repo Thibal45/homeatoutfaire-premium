807a673d175bb4b92173bbaf89d5e04ff0bee8e3
Thibaut Sachot <hatf30190@gmail.com>
2026-08-16 15:27:41 +0200

    Ajout des données structurées SEO

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,120 @@ import Footer from "./components/Footer";
 import BackToTop from "./components/BackToTop";
 import WhatsAppButton from "./components/WhatsAppButton";
 
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "HomeAndConstructionBusiness",
+  "@id": "https://www.homeatoutfaire30.fr/#business",
+
+  name: "HomeAToutFaire",
+
+  url: "https://www.homeatoutfaire30.fr",
+
+  telephone: "+33618085598",
+
+  image: "https://www.homeatoutfaire30.fr/images/logo-homeatoutfaire.png",
+
+  logo: "https://www.homeatoutfaire30.fr/images/logo-homeatoutfaire.png",
+
+  description:
+    "HomeAToutFaire réalise vos petits travaux de plomberie, électricité, peinture, menuiserie, entretien de piscine et dépannage dans un rayon de 50 km autour de Sainte-Anastasie.",
+
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "28 rue du Bel Air",
+    postalCode: "30190",
+    addressLocality: "Sainte-Anastasie",
+    addressRegion: "Occitanie",
+    addressCountry: "FR",
+  },
+
+  areaServed: {
+    "@type": "GeoCircle",
+    geoMidpoint: {
+      "@type": "GeoCoordinates",
+      addressCountry: "FR",
+      addressLocality: "Sainte-Anastasie",
+    },
+    geoRadius: "50000",
+  },
+
+  openingHoursSpecification: [
+    {
+      "@type": "OpeningHoursSpecification",
+      dayOfWeek: [
+        "https://schema.org/Monday",
+        "https://schema.org/Tuesday",
+        "https://schema.org/Wednesday",
+        "https://schema.org/Thursday",
+        "https://schema.org/Friday",
+        "https://schema.org/Saturday",
+      ],
+      opens: "08:00",
+      closes: "18:00",
+    },
+  ],
+
+  hasOfferCatalog: {
+    "@type": "OfferCatalog",
+    name: "Services HomeAToutFaire",
+
+    itemListElement: [
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Petits travaux de plomberie",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Petits travaux électriques",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Peinture",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Menuiserie",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Entretien de piscine",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Dépannage",
+        },
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd),
+        }}
+      />
+
       <ScrollProgress />
       <Header />
       <Hero />
@@ -24,7 +135,7 @@ export default function Home() {
       <Quote />
       <Contact />
       <Footer />
-      <BackToTop/>
+      <BackToTop />
       <WhatsAppButton />
     </>
   );
